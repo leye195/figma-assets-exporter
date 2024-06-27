340d325799d704471e8e94f2b9720aa7d55f8622
leye195 <leye195@naver.com>
2024-06-27 23:32:39 +0900

    feat: add esbuild & apply esbuild on build script

---
+++ esbuild.config.js
@@ -0,0 +1,21 @@
+const esbuild = require("esbuild");
+const { execSync } = require("child_process");
+
+// 'src' 디렉터리에서 'example.ts' 제외한 모든 .ts 파일 찾기
+const tsFiles = execSync("find src -name '*.ts' ! -name 'example.ts'", {
+  encoding: "utf-8",
+})
+  .split("\n")
+  .filter(Boolean);
+
+esbuild
+  .build({
+    entryPoints: tsFiles,
+    platform: "node",
+    target: "esnext",
+    outdir: "dist",
+    format: "cjs",
+    bundle: true,
+    logLevel: "info",
+  })
+  .catch(() => process.exit(1));
