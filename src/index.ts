938d2916622f61fed78687ff58749d3f4df8384d
leye195 <leye195@naver.com>
2024-06-27 21:19:52 +0900

    feat: update build script

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-import { FigmaAssetExporter } from "./figma";
-
-(async () => {
-  const figmaExporter = new FigmaAssetExporter({
-    token: "FIGMA_ACCESS_TOKEN",
-    pageName: "PAGE_NAME",
-    assetsPath: "assets/",
-    fileId: "FIGMA_FILE_ID",
-  });
-
-  const assets = await figmaExporter.getAssets(["838-2908"]);
-  const results = await figmaExporter.exportAssets(assets);
-  figmaExporter.saveAssets(results);
-})();
+export * from "./figma";
